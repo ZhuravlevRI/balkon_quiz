55b7d7ac96be6ba88b09608907f048221fbd994e
kebabulon <3dmod07@mail.ru>
2026-03-09 20:47:41 +0300

    feat: add Tanstack Router, add Tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+
+import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+import path from 'path';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
+        tailwindcss(),
+        react(),
+        // ...,
+    ],
 })

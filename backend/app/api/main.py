cde5cdaecd30631623cee48b76b01398a7f87251
TheFid12 <zhenya.bytchkov@mail.ru>
2026-04-29 18:47:14 +0300

    feat: image routes

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 
 from app.api.routes import (
-    users,
-    quiz,
+    images,
     questions,
+    quiz,
+    users,
 )
 from app.core.config import settings
 
 api_router = APIRouter()
+api_router.include_router(images.router)
 api_router.include_router(users.router)
 api_router.include_router(quiz.router)
 api_router.include_router(questions.router)
